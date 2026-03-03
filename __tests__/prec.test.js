df862b675ec1c5bd1d9bfbedc98ac0b41dfdeab9
Saul Lorenzo Armas <alu0101642468@ull.edu.es>
2026-03-03 21:51:56 +0000

    Añadir el parser

--- __tests__/prec.test.js
+++ __tests__/prec.test.js
@@ -1,3 +1,5 @@
+const { parse } = require('../src/parser');
+
 describe('Parser Failing Tests', () => {
   test('should handle multiplication and division before addition and subtraction', () => {
     expect(parse("2 + 3 * 4")).toBe(14); // 2 + (3 * 4) = 14
